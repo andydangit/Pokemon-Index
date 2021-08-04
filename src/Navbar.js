8446d2f9315fdc91d40bbf2ad1f3429955a51dd5
Andy Dang <andy.dang.it@gmail.com>
2021-08-04 15:03:37 -0500

    update info to Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1> Pokemon Index</h1>
+            <div className="links"> 
+            <a href='/'>Home </a> 
+            <a href='/about'> About </a>
+            </div>
 
         </nav>
      );
