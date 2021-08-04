e4e004b935f68a44273d0ca787bc1caab991877a
Andy Dang <andy.dang.it@gmail.com>
2021-08-04 14:39:13 -0500

    add navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1> Pokemon Index</h1>
+
+        </nav>
+     );
+}
+ 
+export default Navbar;
