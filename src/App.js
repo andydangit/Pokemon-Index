cc86c3b334aed28c99a95e62e4d97b84580dcd35
Andy Dang <andy.dang.it@gmail.com>
2021-08-04 20:06:46 -0500

    update to Router / Route/ Switch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import "./App.css";
 import Navbar from "./Component/Navbar";
+import Home from "./Component/pages/Home";
+import About from "./Component/pages/About";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <Navbar />;
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+         <Switch>
+           <Route exact path="/"> 
+              <Home />
+           </Route>
+           <Route path="/about"> 
+              <About />
+           </Route>
+         </Switch>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
+
