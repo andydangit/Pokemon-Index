e249797364d9b683a874f0ee1a8fe0a5550ac390
Andy Dang <andy.dang.it@gmail.com>
2021-08-05 12:18:11 -0500

    update About field

--- src/Component/pages/About.js
+++ src/Component/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
         <div className="About"> 
             <h1> About </h1> 
         <div>
-            <h2> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam orci leo, maximus ac sapien vitae, blandit tincidunt purus. Mauris blandit. </h2>
+            <h2> Out of 898 pokemon out in the wild; the original 9 is still the best there is and the best there ever will be!!   </h2>
         </div>
         </div> 
      );
