59c501ba52f9b11f578a9807939221820fa23249
Andy Dang <andy.dang.it@gmail.com>
2021-08-04 19:41:37 -0500

    Add content for About

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+
+const About = () => {
+    return ( 
+        <div className="About"> 
+            <h1> About </h1> 
+        <div>
+            <h2> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam orci leo, maximus ac sapien vitae, blandit tincidunt purus. Mauris blandit. </h2>
+        </div>
+        </div> 
+     );
+}
+ 
+export default About;
