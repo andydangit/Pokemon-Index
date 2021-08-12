b63a1196fee33b437b4360d4dc923b4daf748026
Andy Dang <andy.dang.it@gmail.com>
2021-08-12 11:29:13 -0500

    update home page

--- src/Component/pages/Home.js
+++ src/Component/pages/Home.js
@@ -1,11 +1,11 @@
-
-
 import React from 'react'
+import Navbar from "../Navbar"
 
 export default function Home() {
     return (
         <div className="Home">
-            <h1> Home </h1> 
+            <Navbar /> 
+                        
         </div>
     )
 }
