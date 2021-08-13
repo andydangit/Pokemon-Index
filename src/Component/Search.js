d84206b871bcca28531664b0c105004a312bfb77
Andy Dang <andy.dang.it@gmail.com>
2021-08-13 18:20:24 -0500

    Add axios to get pokemon api

--- src/Component/Search.js
+++ src/Component/Search.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Search.css';
+import axios from 'axios';
+
 
 function Search() {
+
+    const [ pokemonName, setPokemonName] = useState("");
+
+     
+
+    const searchPokemon = () => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`). then((response) => {
+            console.log(response);
+        }
+        );
+    }
+
     return (
         <div className="Search">
-            <input type='text' /> 
-            <button>Pokemon Search</button> 
+            <input type='text' onChange ={( e ) =>
+                {setPokemonName( e.target.value)} 
+            }/> 
+            <button onClick={ searchPokemon }>Pokemon Search</button> 
         </div>
     )
 }
